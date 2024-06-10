54ac5ce34db5764d15697a8d81b3e6e7bbdd0170
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-06-10 15:13:46 +0530

    first commit

---
+++ bool.java
@@ -0,0 +1,33 @@
+public class bool {
+
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 56;
+        System.out.println(x>y);
+
+        int x1 = 152;
+        int y1 = 56;
+        System.out.println(x1>y1);
+
+        int z = 10;
+        System.out.println(z == 10);
+
+        System.out.println(10 == 15);
+
+
+        int myAge1 = 15;
+        int votingAge1 = 18;
+        System.out.println(myAge1 >= votingAge1);
+
+        int myAge = 25;
+        int votingAge = 18;
+
+        if (myAge >= votingAge) {
+            System.out.println("Old enough to vote!");
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+    
+    
+    }
+}
